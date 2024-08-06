4bfa39798b60249dc8f5d064aa7568616450a01a
Jon <onaichimhanda@gmail.com>
2024-08-06 23:00:35 +0200

    tari

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+        document.body.classList.remove("container");
+    };
+
+    alert ("Here are your flowers😒😒😒follow instructions" );
+    
+    // Prompt the user for their name
+const userName = prompt("HEY BABE PUT YOUR NAME HERE");
+
+// Check if the user provided a name
+if (userName) {
+  // Display the happy anniversary message with the user's name
+  alert(`Happy Anniversary , ${userName}!,I know this is probably the 10th time I've said this, but I just can't help myself my love for you grows stronger everyday.
+ Your love, support, and understanding have been the cornerstone of my happiness. Each day with you feels like a new adventure, and I cherish every moment we share.
+Thank you for being  my confidant, and my greatest love. I am endlessly grateful for the laughter, the happiness, and the boundless love you give. Here's to many more years of making beautiful memories together❤️😍ILY.`);
+} else {
+  // Display a message if no name was provided
+  alert("Happy Anniversary ");
+}
